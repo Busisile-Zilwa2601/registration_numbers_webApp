cb37d9e4a27c02d80908f8004a55151117f2e22e
Busisile-Zilwa2601 <busisile.box@gmail.com>
2018-09-11 11:33:27 +0200

    added values on database

--- tables_reg_numbers.sql
+++ tables_reg_numbers.sql
@@ -1,3 +1,4 @@
+Drop TABLE if EXISTS towns, plates;
 create table towns( id serial not null primary key,
                     town_name text not null, 
                     town_code text not null
@@ -5,5 +6,9 @@ create table towns( id serial not null primary key,
 create table plates( id serial not null primary key,
                      reg_number text not null, 
                      towns_id int, 
-                     foreign key(towns_id) references towns(id)
+                     foreign key(towns_id) references towns(id) on DELETE CASCADE
 );
+INSERT INTO towns(town_name, town_code) VALUES('Cape Town', 'CA');
+INSERT INTO towns(town_name, town_code) VALUES('Bellville', 'CY');
+INSERT INTO towns(town_name, town_code) VALUES('Paarl', 'CJ');
+INSERT INTO towns(town_name, town_code) VALUES('Stellenbosch', 'CL');
