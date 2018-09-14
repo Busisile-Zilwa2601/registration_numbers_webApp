35c4877fc56d5b151e040b2edcae4f3093d0ef85
Busisile-Zilwa2601 <busisile.box@gmail.com>
2018-09-14 20:36:02 +0200

    added few tests

--- test/registration-number.test.js
+++ test/registration-number.test.js
@@ -16,13 +16,12 @@ describe('The Registration-Numbers database webApp', function () {
     });
     it('all in length: Should return the database length', async function(){
         let reg = reg_numbers(pool);
-        await reg.add('CA 230');
-        await reg.add('CY 930-897');
+        await reg.add('CA 230-009');
         await reg.add('CJ 230');
         await reg.add('CA 230');
-        await reg.add('CL 9804');
+        await reg.add('CL 98045');
         let reg_numbs = await reg.all();
-        console.log(reg_numbs.length);
+        console.log(reg_numbs);
         assert.equal(reg_numbs.length, 4);
     });
     it('allFrom: Should return Cape Town registration numbers that are in the database', async function(){
@@ -31,9 +30,9 @@ describe('The Registration-Numbers database webApp', function () {
         await reg.add('CA 930-897');
         await reg.add('CJ 230');
         await reg.add('CA 980');
-        let reg_numbs = await reg.allFrom('Cape Town');
+        let reg_numbs = await reg.allFrom('CAPE TOWN');
         //console.log(JSON.stringify(reg_numbs));
-        assert.deepEqual(reg_numbs, [{town_name:'Cape Town',reg_number:'CA 230'},{town_name:'Cape Town',reg_number:'CA 930-897'},{town_name:'Cape Town',reg_number:'CA 980'}]);
+        assert.deepEqual(reg_numbs, [{town_name:'CAPE TOWN',reg_number:'CA 230'},{town_name:'CAPE TOWN',reg_number:'CA 930-897'},{town_name:'CAPE TOWN',reg_number:'CA 980'}]);
     });
     it('CheckTag: Should return False if the town_code is not in database', async function(){
         let reg = reg_numbers(pool);
@@ -42,24 +41,56 @@ describe('The Registration-Numbers database webApp', function () {
     });
     it('CheckTag: Should return True if the town_code is in database', async function(){
         let reg = reg_numbers(pool);
-        let reg_numbs = await reg.checkTag('CY');
+        let reg_numbs = await reg.checkTag('CL');
         assert.equal(reg_numbs, true);
     });
-    it('CheckReg: Should return TRUE when registraion-number is not in the database, then it will be added', async function(){
+    it('CheckReg: Should return TRUE when registraion-number is not in the database', async function(){
         let reg = reg_numbers(pool);
         await reg.add('CA 230');
         await reg.add('CY 930-897');
         let reg_numbs = await reg.checkReg('CA 231');
         assert.equal(reg_numbs, true);
     });
-    it('CheckReg: Should return FALSE when registraion-number is in the database, then it will not be added', async function(){
+    it('CheckReg: Should return FALSE when registraion-number is in the database', async function(){
         let reg = reg_numbers(pool);
         await reg.add('CA 230');
         await reg.add('CY 930-897');
         let reg_numbs = await reg.checkReg('CA 230');
         assert.equal(reg_numbs, false);
     });
-    
+    it('checkTown: Should return True when the City is the not in the database', async function(){
+        let reg = reg_numbers(pool);
+        let city = await reg.checkTown('WORCESTER');
+        assert.equal(city, true);
+    });
+    it('checkTown: Should return False when the City is the in the database', async function(){
+        let reg = reg_numbers(pool);
+        let city = await reg.checkTown('CAPE TOWN');
+        assert.equal(city, false);
+    });
+    it('addTown: Should add a the City when is the not in the database, then return True', async function(){
+        let reg = reg_numbers(pool);
+        let cityCape = await reg.addTown('CAPE TOWN', 'CA');
+        assert.deepEqual(cityCape, false);
+    });
+    it('deleteAllReg: Should delete all registration numbers on the database', async function(){
+        let reg = reg_numbers(pool);
+        await reg.add('CA 230-009');
+        await reg.add('CJ 230');
+        await reg.add('CA 230');
+        await reg.add('CL 98045');
+        let reg_numbs = await reg.all();
+        console.log(reg_numbs);
+        await reg.deleteAllReg();
+        console.log(await reg.all());
+        assert.deepEqual(await reg.all(), []);
+    });
+    it('deleteMyTown: Should delete a City when is the in the database', async function(){
+        let reg = reg_numbers(pool);
+        await reg.deleteMyTown('GEORGE');
+        let city = await reg.checkTown('GEORGE');
+        assert.deepEqual(city, true);
+    });
     after(function(){
         pool.end();
     })
